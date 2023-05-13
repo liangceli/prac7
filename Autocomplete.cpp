561952ed4acd89d56aaebb6d6435bd0f58adf522
Liangce Li <liliangce316@gmail.com>
2023-05-13 22:41:10 +0930

    new

--- Autocomplete.cpp
+++ Autocomplete.cpp
@@ -3,8 +3,7 @@
 #include "Autocomplete.h"
 using namespace std;
 
-
-Autocomplete :: Autocomplete(){
+/*Autocomplete :: Autocomplete(){
     root=new TrieNode(); //为Trie tree的根 root分配空间
 }
 
@@ -23,6 +22,7 @@ void Autocomplete :: insert(string word){
      }
 
      node->isEndOfword=true; //插入完成之后将此时的这个字符串的最后一个char设为 true 证明插入完成
+     cout<<"okok";
 }
 
 vector<string> Autocomplete :: getSuggestions(string partialWord){ //当输入partialword的时候提供提示 如果输入‘c’ 如果tree里面有“cat” “car” 存储在vector中并且打印出来
@@ -71,4 +71,69 @@ vector<string> Autocomplete :: getSuggestions(string partialWord){ //当输入pa
 
     return suggestions;
 }
+
+Autocomplete::~Autocomplete(){
+    delete root;
+}*/
+void Autocomplete::insert(const std::string& word) {
+    insert_helper(root, word);
+}
+
+void Autocomplete::insert_helper(TrieNode* node, const std::string& word) {
+    if (word.empty()) {
+        node->is_word = true;
+        return;
+    }
+    
+    char c = tolower(word[0]); // convert character to lowercase
+    int idx = c - 'a';
+    if (!node->children[idx]) {
+        node->children[idx] = new TrieNode();
+    }
+    insert_helper(node->children[idx], word.substr(1));
+}
+
+// Returns a vector of words that start with the given partial word
+std::vector<std::string> Autocomplete::getSuggestions(const std::string& partialWord) const {
+    std::vector<std::string> suggestions;
+    TrieNode* node = find_node(partialWord);
+    if (node) {
+        get_suggestions_helper(node, suggestions, partialWord);
+    }
+    return suggestions;
+}
+
+// Returns the node that represents the given word, or null if the word is not in the Trie
+TrieNode* Autocomplete::find_node(const std::string& word) const {
+    TrieNode* node = root;
+    for (char c : word) {
+        int idx = tolower(c) - 'a'; // convert character to lowercase and get index in children array
+        if (!node->children[idx]) {
+            return nullptr; // word not in Trie
+        }
+        node = node->children[idx];
+    }
+    return node;
+}
+
+// Recursively adds words to the suggestions vector that start with the prefix represented by node
+void Autocomplete::get_suggestions_helper(TrieNode* node, std::vector<std::string>& suggestions, std::string& prefix) const {
+    if (node->is_word) {
+        suggestions.push_back(prefix);
+    }
+    
+    for (int i = 0; i < 26; i++) {
+        if (node->children[i]) {
+            prefix.push_back(i + 'a');
+            get_suggestions_helper(node->children[i], suggestions, prefix);
+            prefix.pop_back();
+        }
+    }
+}
+
+
+
+
+
+
 
